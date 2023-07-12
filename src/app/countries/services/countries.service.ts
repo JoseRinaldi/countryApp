24ef80bfe0af118266594612137a61acccac1d3d
José María Rinaldi <115929308+JoseRinaldi@users.noreply.github.com>
2023-07-12 12:48:27 -0300

    debouncer

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -17,7 +17,8 @@ export class CountriesService {
     return this.httpClient.get<Country[]>(url)
     .pipe(
       catchError( () => of([])),
-      delay( 2000 ));
+      //delay( 2000 )
+      );
 
   }
 
